21c4491364c60b1a7c56ed97a459d6aaf755d245
hamai <mhamail1223@gmail.com>
2023-08-15 13:12:54 +0500

    adding

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 
 
 app.use("/",(req,res)=>{res.send("home page")})
