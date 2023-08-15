8590b3292a073231ac28597622a3243837e029bb
hamai <mhamail1223@gmail.com>
2023-08-15 21:45:23 +0500

    ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 require('./dbconn')
+const Product = require("./models/product")
 
 
 const app = express()
@@ -11,7 +12,12 @@ const port = process.env.PORT || 7000;
 
 
 app.get("/",(req,res)=>{res.send("home page")})
-app.get("/api",(req,res)=>{res.send("mhmarket")})
+app.get("/api",(req,res)=>{
+    Product.find({})
+    .then((data)=>res.json(data))
+    .catch(err=>res.json(err))
+
+})
 
 console.log("mhmarket mhmarket")
 
