0c3ae4b558f65aef4a270295f71bb01eb0ced094
hamai <mhamail1223@gmail.com>
2023-08-16 15:36:21 +0500

    ok

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,7 @@ const Product = require('../models/product');
 exports.listFeatured = (req, res) => {
     Product.find({ featured: true })
         .limit(24)
-        .then((res) => {
+        .then((product) => {
             res.json(product)
         })
         .catch(err => {
